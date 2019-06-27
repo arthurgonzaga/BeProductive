f7a00ce32d0e2f9a769b72dddf619d7741d9b72c
Arthur Gonzaga <arthurbilul@gmail.com>
2019-06-27 17:35:08 -0300

    new warning folder

---
+++ warningBreak/time.js
@@ -0,0 +1,26 @@
+let timeLeft = 60 * 5;
+var timer = document.querySelector('#timer');
+var count = 0;
+
+function convertSeconds(second){
+    var minute = Math.floor(second / 60);
+    var sec = second % 60;
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+    if(minute <10){
+        minute = "0" + minute;
+    }
+    return minute + ":" + sec;
+}
+window.addEventListener('keydown', ()=>{
+    console.log(ev.code);
+})
+window.onload = () => {
+    timer.innerHTML = convertSeconds(timeLeft - count);
+    count++
+    setInterval(() =>{
+        timer.innerHTML = convertSeconds(timeLeft - count);
+        count++;
+    }, 1000);
+}
