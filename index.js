b7310c1d81d166dc6ea27b9839006a338116176c
Arthur Gonzaga <arthurbilul@gmail.com>
2019-06-27 17:38:23 -0300

    change variable names

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const electron = require('electron')
 const {app, BrowserWindow, globalShortcut} = require('electron')
 
+let timeAlive = (60 * 5) * 1100
 var closable = false
-let window;
-const onAppReady = function () {
-    window= new BrowserWindow({
+let warningBreak;
+
+const openWarningBreak = function () {
+    warningBreak= new BrowserWindow({
         width: 400,
         height: 400,
         frame: false,
@@ -13,8 +15,8 @@ const onAppReady = function () {
         fullscreen: true,
         closable:false,
     })
-    window.loadFile('index.html')
-    window.maximize()
+    warningBreak.loadFile('warningBreak/warning.html')
+    warningBreak.maximize()
     globalShortcut.register('F11', () => {})
     globalShortcut.register('Alt+F4', () =>{})
     globalShortcut.register('Alt+Tab', () =>{})
@@ -23,17 +25,16 @@ const onAppReady = function () {
     globalShortcut.register('CommandOrControl+r', () =>{})
     globalShortcut.register('CommandOrControl+Shift+r', () =>{})
     globalShortcut.register('CommandOrControl+Shift+i', () =>{})
-    // uncomment later
-    //globalShortcut.register('CommandOrControl+q', () =>{})
+    globalShortcut.register('CommandOrControl+q', () =>{})
     setTimeout(() => {
         closable = true
-        window.close()
-    }, (1 * 5) * 1000);
-    window.on('close', (event) =>{
+        warningBreak.close()
+    }, timeAlive);
+    warningBreak.on('close', (event) =>{
         if(closable == false){
             event.preventDefault()
         }
     })
 };
 
-app.on('ready', () => setTimeout(onAppReady,500))
+app.on('ready', () => setTimeout(openWarningBreak,500))
